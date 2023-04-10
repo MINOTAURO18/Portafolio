f8a1917393fdea9f317aa09b2adce15bcd562b7f
alexander muñoz <alexander12345my@gmail.com>
2023-04-10 09:41:55 -0500

    inicio-mobile

--- src/components/Inicio/Botones/Botones.jsx
+++ src/components/Inicio/Botones/Botones.jsx
@@ -4,9 +4,9 @@ const Botones = () => {
     return(
       <div className={styles.Botones}>
        <button><a className={styles.a} href="#">CV</a></button>
-       <button><a className={styles.a} href="#">Contacto</a></button>
+       <button><a className={styles.a} href="#">GMAIL</a></button>
       </div> 
     )
 }
-
+ 
 export default Botones; 
