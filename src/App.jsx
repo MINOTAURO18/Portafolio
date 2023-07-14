4a13f841762cf7d94859e7e8850436071d814e97
alexander muñoz <alexander12345my@gmail.com>
2023-07-14 11:05:49 -0500

    route animation

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Proyectos from './components/Proyectos/Proyectos';
 import SobreMi from './components/SobreMi/SobreMi';
 import Nav from './components/Nav/Nav';
 import {AnimatePresence} from 'framer-motion'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 
 function App() {
@@ -13,15 +14,34 @@ function App() {
 
   return (
     <div className="App">
-      <AnimatePresence>
       <Nav/>
 
-        <Routes location={location} key={location.pathname}>  
-        <Route path='/portafolio/' element={<Inicio/>}/>   
-        <Route path='/portafolio/proyectos' element={<Proyectos/>}/>
-        <Route path='/portafolio/sobremi' element={<SobreMi/>}/>
+        <Routes >  
+        <Route  path="/portafolio/"
+          element={
+            <TransitionGroup>
+              <CSSTransition key="home" classNames="fade" timeout={300}>
+                <Inicio/>
+              </CSSTransition>
+            </TransitionGroup>
+          }/>   
+        <Route  path="/portafolio/proyectos"
+          element={
+            <TransitionGroup>
+              <CSSTransition key="proyectos" classNames="fade" timeout={300}>
+                <Proyectos />
+              </CSSTransition>
+            </TransitionGroup>
+          }/>
+        <Route path="/portafolio/sobremi"
+          element={
+            <TransitionGroup>
+              <CSSTransition key="about" classNames="fade" timeout={300}>
+                <SobreMi />
+              </CSSTransition>
+            </TransitionGroup>
+          }/>
         </Routes>
-      </AnimatePresence>
       
     </div>
   )
